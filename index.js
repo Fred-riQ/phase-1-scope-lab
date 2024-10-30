e0221162132f63e4ca3c27d0c1acc74a28f65b88
Fred-riQ <fredrickbet5@gmail.com>
2024-10-30 13:45:51 +0300

    update file

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName= 'bob';
+function customerName(){
+    
+}
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase(); // Convert to uppercase
+
+ } // Return the modified customerName
+ 
+ function setBestCustomer(){
+    bestCustomer = 'not bob';
+ }
+ 
+function overWriteBestCustomer(){
+
+    bestCustomer= 'maybe bob';
+
+}
+const leastFavoriteCustomer= 'some value';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'new value';
+}
